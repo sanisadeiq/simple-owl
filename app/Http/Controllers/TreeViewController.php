69303ac83ae8ba890ce24423b0f05ebf75da7a0b
skmll <rafael.costa90@gmail.com>
2015-03-27 17:30:48 +0000

    adsasd

--- app/Http/Controllers/TreeViewController.php
+++ app/Http/Controllers/TreeViewController.php
@@ -30,75 +30,68 @@ class TreeViewController extends Controller {
 	 */
 	public function index()
 	{
-
+		
 		$xml = simplexml_load_file("example.xml") or die("Error: Cannot create object");
 
 		$i = 1;
 		$ids = array();
 		$ids['Thing'] = 0;
-		$results = array();
+		$items = array();
 
 		foreach ($xml->class as $class) {
-			$ids[sprintf('%s', $class->name)] = $i;
+			//print_r($class['name'.""]. '<br>');
+			$ids[$class['name'].""] = $i;
 			$i++;
 		}
 
-		foreach ($xml->children() as $class) {
-			//echo sprintf('CLASS: %s', $class->name) . "<br>";
-			array_push($results, Array(
-				'id' => $ids[sprintf('%s', $class->name)],
-				'title' => $class->name . "",
-				'parent_id' => $ids[sprintf('%s', $class->parentClass)]
+		foreach ($xml->class as $class) {
+			//echo sprintf('parent: %s', $class['parent']) . "<br>";
+			array_push($items, Array(
+				'id' => $ids[$class['name'].""],
+				'title' => $class['name'] . "",
+				'parent_id' => $ids[$class['parent'].""]
 				));
-			foreach ($class->individuals as $inds) {
-				foreach ($inds->children() as $ind) {
-					//echo sprintf('CLASS: %s -> INDIVIDUAL: %s', $class->name, $ind->name) . "<br>";
-					array_push($results, Array(
+			foreach ($class->individual as $ind) {
+				//echo sprintf('CLASS: %s -> INDIVIDUAL: %s', $class->name, $ind->name) . "<br>";
+				array_push($items, Array(
+					'id' => $i,
+					'title' => $ind['name'] . "",
+					'parent_id' => $ids[$class['name'].""]
+					));
+				$lastInd = $i;
+				$i++;
+				foreach ($ind->relationship as $rel) {
+					//echo sprintf('CLASS: %s -> INDIVIDUAL: %s -> RELATIONSHIP: %s',
+					 //$class->name, $ind->name, $rel->name) . "<br>";
+					array_push($items, Array(
 						'id' => $i,
-						'title' => $ind->name . "",
-						'parent_id' => $ids[sprintf('%s', $class->name)]
-						));
-					$lastInd = $i;
-					$i++;
-					foreach ($ind->relationships as $rels) {
-						foreach ($rels->children() as $rel) {
-							//echo sprintf('CLASS: %s -> INDIVIDUAL: %s -> RELATIONSHIP: %s',
-							 //$class->name, $ind->name, $rel->name) . "<br>";
-							array_push($results, Array(
-								'id' => $i,
-								'title' => $rel->name . "",
-								'parent_id' => $lastInd
-								));
-							$i++;
-						}
-					}
-				}
-			}
-			foreach ($class->necessaryConditions as $necs) {
-				foreach ($necs->children() as $nec) {
-					//echo sprintf('CLASS: %s -> NECCONDITION: %s', $class->name, $nec->condition) . "<br>";
-					array_push($results, Array(
-						'id' => $i,
-						'title' => $nec->condition . "",
-						'parent_id' => $ids[sprintf('%s', $class->name)]
+						'title' => $rel['name'] . " " . $rel,
+						'parent_id' => $lastInd
 						));
 					$i++;
 				}
 			}
-			foreach ($class->sufficientConditions as $sufs) {
-				foreach ($sufs->children() as $suf) {
-					//echo sprintf('CLASS: %s -> SUFCONDITION: %s', $class->name, $suf->condition) . "<br>";
-					array_push($results, Array(
-						'id' => $i,
-						'title' => $suf->condition . "",
-						'parent_id' => $ids[sprintf('%s', $class->name)]
-						));
-					$i++;
-				}
+			foreach ($class->necessaryCondition as $nec) {
+				//echo sprintf('CLASS: %s -> NECCONDITION: %s', $class->name, $nec->condition) . "<br>";
+				array_push($items, Array(
+					'id' => $i,
+					'title' => $nec['name'] . "",
+					'parent_id' => $ids[sprintf('%s', $class['name'])]
+					));
+				$i++;
+			}
+			foreach ($class->sufficientCondition as $suf) {
+				//echo sprintf('CLASS: %s -> SUFCONDITION: %s', $class->name, $suf->condition) . "<br>";
+				array_push($items, Array(
+					'id' => $i,
+					'title' => $suf['name'] . "",
+					'parent_id' => $ids[sprintf('%s', $class['name'])]
+					));
+				$i++;
 			}
 		}
 
-		return view('treeview', ['items' => $results]);
+		return view('treeview', ['items' => $items]);
 	}
 
 	public function getInitialTree()
@@ -123,85 +116,87 @@ class TreeViewController extends Controller {
 			sleep(1);
 		}
 
-		$xml = simplexml_load_file("example.xml") or die("Error: Cannot create object");
+				$xml = simplexml_load_file("example.xml") or die("Error: Cannot create object");
 
 		$i = 1;
 		$ids = array();
 		$ids['Thing'] = 0;
-		$results = array();
+		$items = array();
 
 		foreach ($xml->class as $class) {
-			$ids[sprintf('%s', $class->name)] = $i;
+			//print_r($class['name'.""]. '<br>');
+			$ids[$class['name'].""] = $i;
 			$i++;
 		}
 
-		foreach ($xml->children() as $class) {
-			//echo sprintf('CLASS: %s', $class->name) . "<br>";
-			array_push($results, Array(
-				'id' => $ids[sprintf('%s', $class->name)],
-				'title' => $class->name . "",
-				'parent_id' => $ids[sprintf('%s', $class->parentClass)]
+		foreach ($xml->class as $class) {
+			//echo sprintf('parent: %s', $class['parent']) . "<br>";
+			array_push($items, Array(
+				'id' => $ids[$class['name'].""],
+				'title' => $class['name'] . "",
+				'parent_id' => $ids[$class['parent'].""],
+				'active' => true
 				));
-			foreach ($class->individuals as $inds) {
-				foreach ($inds->children() as $ind) {
-					//echo sprintf('CLASS: %s -> INDIVIDUAL: %s', $class->name, $ind->name) . "<br>";
-					array_push($results, Array(
+			foreach ($class->individual as $ind) {
+				//echo sprintf('CLASS: %s -> INDIVIDUAL: %s', $class->name, $ind->name) . "<br>";
+				array_push($items, Array(
+					'id' => $i,
+					'title' => $ind['name'] . "",
+					'parent_id' => $ids[$class['name'].""],
+				'active' => true
+					));
+				$lastInd = $i;
+				$i++;
+				foreach ($ind->relationship as $rel) {
+					//echo sprintf('CLASS: %s -> INDIVIDUAL: %s -> RELATIONSHIP: %s',
+					 //$class->name, $ind->name, $rel->name) . "<br>";
+					array_push($items, Array(
 						'id' => $i,
-						'title' => $ind->name . "",
-						'parent_id' => $ids[sprintf('%s', $class->name)]
-						));
-					$lastInd = $i;
-					$i++;
-					foreach ($ind->relationships as $rels) {
-						foreach ($rels->children() as $rel) {
-							//echo sprintf('CLASS: %s -> INDIVIDUAL: %s -> RELATIONSHIP: %s',
-							 //$class->name, $ind->name, $rel->name) . "<br>";
-							array_push($results, Array(
-								'id' => $i,
-								'title' => $rel->name . "",
-								'parent_id' => $lastInd
-								));
-							$i++;
-						}
-					}
-				}
-			}
-			foreach ($class->necessaryConditions as $necs) {
-				foreach ($necs->children() as $nec) {
-					//echo sprintf('CLASS: %s -> NECCONDITION: %s', $class->name, $nec->condition) . "<br>";
-					array_push($results, Array(
-						'id' => $i,
-						'title' => $nec->condition . "",
-						'parent_id' => $ids[sprintf('%s', $class->name)]
+						'title' => $rel['name'] . " " . $rel,
+						'parent_id' => $lastInd,
+						'active' => true
 						));
 					$i++;
 				}
 			}
-			foreach ($class->sufficientConditions as $sufs) {
-				foreach ($sufs->children() as $suf) {
-					//echo sprintf('CLASS: %s -> SUFCONDITION: %s', $class->name, $suf->condition) . "<br>";
-					array_push($results, Array(
-						'id' => $i,
-						'title' => $suf->condition . "",
-						'parent_id' => $ids[sprintf('%s', $class->name)]
-						));
-					$i++;
-				}
+			foreach ($class->necessaryCondition as $nec) {
+				//echo sprintf('CLASS: %s -> NECCONDITION: %s', $class->name, $nec->condition) . "<br>";
+				array_push($items, Array(
+					'id' => $i,
+					'title' => $nec['name'] . "",
+					'parent_id' => $ids[sprintf('%s', $class['name'])],
+					'active' => true
+					));
+				$i++;
+			}
+			foreach ($class->sufficientCondition as $suf) {
+				//echo sprintf('CLASS: %s -> SUFCONDITION: %s', $class->name, $suf->condition) . "<br>";
+				array_push($items, Array(
+					'id' => $i,
+					'title' => $suf['name'] . "",
+					'parent_id' => $ids[sprintf('%s', $class['name'])],
+					'active' => true
+					));
+				$i++;
 			}
 		}
-/*
-		$output = array();
-		
-		$handle = fopen($outputfile, "r");
-		if ($handle) {
-			while (($line = fgets($handle)) !== false) {
-				array_push($output, $line);
+
+		foreach ($items as $item) {
+			$item['subs'] = array();
+			$indexedItems[$item['id']] = (object) $item;
+		}
+
+
+		//assign to parent
+		$topLevel = array();
+		foreach ($indexedItems as $item) {
+			if ($item->parent_id == 0) {
+				$topLevel[] = $item;
+			} else {
+				$indexedItems[$item->parent_id]->subs[] = $item;
 			}
-			fclose($handle);
-		} else {
-			array_push($output, "Error openning output file");
-		} 
-*/
-		return $results;
+		}
+
+		return $topLevel;
 	}
 }
